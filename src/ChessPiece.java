9b8d69e36d512c7329b1cc5c847456a5dd245da5
RomaRays <rnkalinin@yandex.ru>
2021-11-29 21:36:27 +0300

    task1complete

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -1,3 +1,12 @@
-public class ChessPiece {
-    
+abstract public class ChessPiece {
+    private String color;
+    private boolean check = true;
+
+    public ChessPiece(String color) {
+        this.color = color;
+    }
+
+    abstract public String getColor();
+    abstract public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+    abstract public String getSymbol();
 }
