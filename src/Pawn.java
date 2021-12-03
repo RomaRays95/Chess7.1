d557fc586bf33b5f05cb901a0a425d08dbd9ef4c
RomaRays <rnkalinin@yandex.ru>
2021-12-03 19:20:50 +0300

    All done!

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,4 +1,4 @@
-public class Pawn extends ChessPiece{
+public class Pawn extends ChessPiece {
 
     public Pawn(String color) {
         super(color);
@@ -12,40 +12,39 @@ public class Pawn extends ChessPiece{
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
         boolean moving = false;
-        if ((toLine >= 0 && toLine <= 7) && (toColumn >= 0 && toColumn <= 7)) {
-            if (line != toLine && column == toColumn) {
-                if (chessBoard.board[toLine][toColumn] == null) {
-                    if (color.equals("White")) {
-                        if (toLine - line == 1) moving = true;
-                        else {
-                            moving = ((toLine - line == 2) && (line == 1) &&
-                                    (chessBoard.board[toLine - 1][toColumn] == null));
-                        }
-                    } else {
-                        if (toLine - line == -1) moving = true;
-                        else {
-                            moving = ((toLine - line == -2) && (line == 6) &&
-                                    (chessBoard.board[toLine + 1][toColumn] == null));
-                        }
+        if (!((toLine >= 0 && toLine <= 7) && (toColumn >= 0 && toColumn <= 7))) return false;
+        if (line != toLine && column == toColumn) {
+            if (chessBoard.board[toLine][toColumn] == null) {
+                if (color.equals("White")) {
+                    if (toLine - line == 1) moving = true;
+                    else {
+                        moving = ((toLine - line == 2) && (line == 1) &&
+                                (chessBoard.board[toLine - 1][toColumn] == null));
+                    }
+                } else {
+                    if (toLine - line == -1) moving = true;
+                    else {
+                        moving = ((toLine - line == -2) && (line == 6) &&
+                                (chessBoard.board[toLine + 1][toColumn] == null));
                     }
                 }
             }
         }
-        return moving || canAttack(chessBoard, line, column, toLine, toColumn);
+        boolean attacking = false;
+        if (chessBoard.board[toLine][toColumn] != null) {
+            if (!chessBoard.board[toLine][toColumn].getColor().equals(color)) {
+                attacking = canAttack(chessBoard, line, column, toLine, toColumn);
+            }
+        }
+        return moving || attacking;
     }
 
-    public boolean canAttack (ChessBoard chessBoard, int line, int column, int toLine, int toColumn){
+    public boolean canAttack(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
         boolean result = false;
-        if ((toLine >= 0 && toLine <= 7) && (toColumn >= 0 && toColumn <= 7)) {
-            if (chessBoard.board[toLine][toColumn] != null) {
-                if (!chessBoard.board[toLine][toColumn].getColor().equals(color)) {
-                    if (color.equals("White")) {
-                        result = ((toLine - line == 1) && (Math.abs(toColumn - column) == 1));
-                    } else {
-                        result = ((toLine - line == -1) && (Math.abs(toColumn - column) == 1));
-                    }
-                }
-            }
+        if (color.equals("White")) {
+            result = ((toLine - line == 1) && (Math.abs(toColumn - column) == 1));
+        } else {
+            result = ((toLine - line == -1) && (Math.abs(toColumn - column) == 1));
         }
         return result;
     }
